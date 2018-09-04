2a7122c275c12bc6cf89e3d2c335a9aa38fbb680
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-04 01:46:10 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :products
   get '/products/:id/inventory', to: 'products#inventory'
   get '/products/:id/description', to: 'products#description'
+  get '/products/:id/index', to: 'products#index'
 end
